a05730efe9a7c610126c8bfae5be741851a8bfad
Suleman Malik <suleman.malik@mail.mcgill.ca>
2019-11-16 12:16:15 -0500

    Add basic queue implementation

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import Level from 'react-bulma-components/lib/components/level';
+import { Field, Control, Input } from 'react-bulma-components/lib/components/form';
+import Heading from 'react-bulma-components/lib/components/heading';
+import Button from 'react-bulma-components/lib/components/button';
+import Box from 'react-bulma-components/lib/components/box';
+import Section from 'react-bulma-components/lib/components/section'
+
+import Queue from '../components/Queue'
+
+const ClinicSection = () => {
+  return (
+      <Section>
+          <Box>
+              <Level>
+                  <Level.Side align="left">
+                      <Level.Item>
+                          <Heading size={5} subtitle>aja</Heading>
+                      </Level.Item>
+                  </Level.Side>
+
+                  <Level.Side align="right" className="is-fullwidth">
+                      <Level.Item>
+                      <Heading size={5} subtitle>aja</Heading>
+
+                      </Level.Item>
+                  </Level.Side>
+              </Level>
+          </Box>
+      </Section>
+  )
+};
 
 const Admin = () => {
   return (
-    <h1> Admin </h1>
+    <Section >
+      <h1> Clinics </h1>
+      <ClinicSection />
+
+      <h1> Queue </h1>
+      <Queue />
+    </Section>
   )
 }
 
