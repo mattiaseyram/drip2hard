bee702f62eece1f1494a9ddb71324189d163b880
Suleman Malik <suleman.malik@mail.mcgill.ca>
2019-11-16 17:13:55 -0500

    push

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -3,57 +3,43 @@ import Box from 'react-bulma-components/lib/components/box';
 import Section from 'react-bulma-components/lib/components/section'
 import { FirestoreContext } from '../utils/context'
 import QueueItem from './QueueItem'
+import QueueItemNew from './QueueItemNew'
 
 import Content from 'react-bulma-components/lib/components/content';
 
-
-const data = [
-  {
-    name: 'Test user',
-    uid: '39284fnh12u383b',
-    email: 'user@mail.com',
-    userType: 'user',
-    data: {
-      timeRemaining: '45 minutes',
-      severity: '90',
-    }
-  },
-  {
-    name: 'another user',
-    uid: '29487fdnjsiahf',
-    email: 'user@mail.com',
-    userType: 'user',
-    data: {
-      timeRemaining: '15 minutes',
-      severity: '30',
-    }
-  },
-  {
-    name: 'dog',
-    uid: 's89f7a98fnds2',
-    email: 'user@mail.com',
-    userType: 'user',
-    data: {
-      timeRemaining: '4 minutes',
-      severity: '10',
-    }
-  }
-]
-
-
 const Queue = () => {
-  const { visits, profiles } = useContext(FirestoreContext)
+  const { visits, profiles, clinic } = useContext(FirestoreContext)
   const visitsArr = visits && Object.values(visits)
 
-  console.log("VIsits: ", visitsArr, profiles)
+  const queueItems = []
+
+  visitsArr && visitsArr.forEach(visit => {
+    const queueItem = {
+      reason: visit.reason,
+      time: visit.time,
+      visitId: visit.id,
+      clinicId: visit.clinic_id,
+      user: {
+        id: visit.user_id,
+        email: profiles[visit.user_id].email,
+        nickname: profiles[visit.user_id].nickname,
+      }
+    }
+    queueItems.push(queueItem)
+  })
+
+  console.log("queue itemssss", queueItems)
+
   return (
     <Section>
       <Content>
         <h2>Upcoming visits</h2>
       </Content>
       {
-        visitsArr && visitsArr.map(visit => (
+        queueItems && queueItems.map(visit => (
+          visit.clinicId == clinic.id ?
           <QueueItem data={visit} key={visit.id} />
+          : null
         ))
       }
     </Section>
--- src/components/QueueItemNew.js
+++ src/components/QueueItemNew.js
@@ -9,6 +9,35 @@ import Box from 'react-bulma-components/lib/components/box';
 const QueueItem = props => {
   const { user, visits, profiles, clinic } = useContext(FirestoreContext);
 
+  console.log('queue item profiles - new', profiles)
+  console.log('queue item visits - new', visits)
+  console.log('q item props - new', props)
+
+  console.log('current clinic - new', clinic)
+
+  const visitsArr = visits && Object.values(visits)
+
+  console.log("visits__arr", visitsArr)
+
+  console.log('NEW QUEEUE ITEM PROPS', props)
+
+  // const queueItems = []
+
+  // visitsArr.forEach(visit => {
+  //   const queueItem = {
+  //     reason: visit.reason,
+  //     time: visit.time,
+  //     visitId: visit.id,
+  //     clinicId: visit.clinic_id,
+  //     user: {
+  //       id: visit.user_id,
+  //       email: profiles[visit.user_id].email,
+  //       nickname: profiles[visit.user_id].nickname,
+  //     }
+  //   }
+  //   queueItems.push(queueItem)
+  // })
+
 
   return (
     <Box className="level-hover">
@@ -21,7 +50,7 @@ const QueueItem = props => {
 
         <Level.Side align="center">
           <Level.Item>
-            <Heading size={6} subtitle>{props.data.reason}</Heading>
+            <Heading size={5} subtitle>{props.data.reason}</Heading>
           </Level.Item>
         </Level.Side>
 
